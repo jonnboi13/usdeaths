f1e3bdcb68a32761b75d8d161ee0386a4802d9ed
J. Hathaway <hathawayj@byui.edu>
2026-01-15 11:34:58 -0700

    Initial commit

---
+++ R/example.r
@@ -0,0 +1,7 @@
+#' An example addition function
+#' @param x1 is the first number in the addition
+#' @param x2 is the second number in the addition
+#' @export 
+add2 <- function(x1, x2) {
+  x1 + x2
+}
